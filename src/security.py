fec96f78474fca1ebafda8c4840d36ef3a58541e
MariosVisos <visos.marios@gmail.com>
2020-04-21 23:59:57 +0200

    Add find by username and find by id

--- src/security.py
+++ src/security.py
@@ -2,23 +2,12 @@ from werkzeug.security import safe_str_cmp
 from user import User
 
 
-# In memory table of registered users
-users = [
-    User(1, 'user1', '12345'),
-    User(2, 'user2', '12345'),
-]
-
-# set comprehension
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-
-
 def authenticate(username, password):
-    user = username_table.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_table.get(user_id, None)
+    return User.find_by_id(user_id)
