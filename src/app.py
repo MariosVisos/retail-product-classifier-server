ae43516b4c0f2106cbd370b56bc8c1de1b4128b8
MariosVisos <visos.marios@gmail.com>
2020-04-25 19:22:05 +0200

    Add dev and prod config fies as well ass environment variables

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
 from db import db
 from blacklist import BLACKLIST
@@ -10,15 +12,9 @@ from resources.image import Image, ImageList
 from resources.dataset import Dataset, DatasetList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["JWT_BLACKLIST_ENABLED"] = True  # enable blacklist feature
-app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = [
-    "access",
-    "refresh",
-]  # allow blacklisting for access and refresh tokens
-app.secret_key = "marios"  # could do app.config['JWT_SECRET_KEY'] if we prefer
+load_dotenv(".env", verbose=True)
+app.config.from_object("default_config")
+app.config.from_envvar("APPLICATION_SETTINGS")
 api = Api(app)
 
 
